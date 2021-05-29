28dc741d512b3b8f239eda106a7f6b05079c0d5f
Robert Hahn <877263+Maschina@users.noreply.github.com>
2021-05-29 17:15:09 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MbSwiftUIFirstResponder",
+    name: "SwiftUIFirstResponder",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v14)
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MbSwiftUIFirstResponder",
-            targets: ["MbSwiftUIFirstResponder"]),
+            name: "SwiftUIFirstResponder",
+            targets: ["SwiftUIFirstResponder"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MbSwiftUIFirstResponder",
+            name: "SwiftUIFirstResponder",
             dependencies: []),
         .testTarget(
-            name: "MbSwiftUIFirstResponderTests",
-            dependencies: ["MbSwiftUIFirstResponder"]),
+            name: "SwiftUIFirstResponder",
+            dependencies: ["SwiftUIFirstResponder"]),
     ]
 )
